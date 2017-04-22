64e0f8cf89db4ef2f8a18e1d927c79d9b25ff396
Sanyyouisf <sanymamdoh@yahoo.com>
2017-04-21 19:24:34 -0500

    dispaly padges

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,25 +2,13 @@ $(document).ready(function() {
 
     //when bushing the button
 
-    
-    // const loadsanyyousif = () => {
-    //     return new Promise((resolve, reject) => {
-    //         $.ajax("https://teamtreehouse.com/sanyyousif.json")
-    //             .done((data1) => {
-    //                 resolve(data1);
-    //                 console.log("data1 sanyyousif", data1);
-    //             })
-    //             .fail((error) => reject(error));
-    //     });
-    // };
-
     $("#btn").on("click", function() {
-
-
     	var input2 = $("#input2").val();
     	var input1 = $("#input1").val();
+    	
 
-	    const loadsanyyousif = () => {
+
+	    const loadFirstPlayer = () => {
 	        return new Promise((resolve, reject) => {
 	            $.ajax("https://teamtreehouse.com/"+input1+".json")
 	            // $.ajax("https://teamtreehouse.com/sanyyousif.json")
@@ -32,18 +20,7 @@ $(document).ready(function() {
 	        });
 	    };
 
-    // const loadellisthomas = () => {
-    //     return new Promise((resolve, reject) => {
-    //         $.ajax("https://teamtreehouse.com/ellisthomas.json")
-    //             .done(function(data1) {
-    //                 resolve(data1);
-    //                 console.log("data1 ellisthomas", data1);
-    //             })
-    //             .fail((error) => reject(error));
-    //     });
-    // };
-   
-	    const loadellisthomas = () => {	
+	    const loadSecondPlayer = () => {	
 	        return new Promise((resolve, reject) => {
 	            $.ajax("https://teamtreehouse.com/"+input2+".json")
 	            // $.ajax("https://teamtreehouse.com/ellisthomas.json")
@@ -56,59 +33,69 @@ $(document).ready(function() {
 	    };
 
 
-    // $("#btn").on("click", function() {
-        // loadsanyyousif();
-        // loadellisthomas();
         var players = [];
-
+        //print every player image and points 
 	    const printplayerData = (players) => {
 	    	console.log("strat print to dom");
 			var playerString = "";
 			for (i=0 ; i<players.length ; i++){
 				playerString += `<div class= "player row col-md-5">`;
-				// playerString +=` <div class= "col-md-5">`;
 				playerString += `<img class="image" src="${players[i].gravatar_url}"`;
 				playerString += `<br>`;
-				playerString += `<p class="points"> the total points is : ${players[i].points.total} </p>`;
+				playerString += `<p class="points"> total points : ${players[i].points.total} </p>`;
 				playerString += `</div>`;
-				// playerString += `</div>`;
-				 	
 			}
 			$("#output").append(playerString);
 			console.log("end print to dom") ;
 		};
 
-		playerPointsArray = [];
+		//to print the winner points and badges
+		var playerPointsArray = [];
+		var winner = "";
 		const checkForWinner = (players) => {
 			console.log("strat check For Winner");
 			playerPointsstrings="";
-			for ( var i=0 ; i< players.length ; i++ ){
+			for (var i=0 ; i< players.length ; i++){
 				playerPointsstrings = players[i].points.total;
-				console.log("playerPointsstrings",playerPointsstrings);
-				console.log("players[i].points.total",players[i].points.totals);
+				// console.log("playerPointsstrings",playerPointsstrings);
+				// console.log("players[i].points.total",players[i].points.totals);
 				playerPointsArray.push(playerPointsstrings) ;
-
 			}
 			if (playerPointsArray[0] > playerPointsArray[1]){
 				console.log("the winner is ",players[0].name);
-				$("#winner").text("the winner is"+(players[0].name));	
+				$("#winner").val("the winner   "+(players[0].name));
+				winner = 0;
 			}
 			else {
 				console.log("the winner is ",players[1].name);
-				$("#winner").text("the winner is"+(players[1].name));
+				$("#winner").val("the winner   "+(players[1].name));
+				winner = 1;	
 			}
-			console.log("playerPointsArray",playerPointsArray);
+			// console.log("playerPointsArray",playerPointsArray);
+			winnerbagdges(players,winner);
 		};
+		//to display winner badges
+		var winnerBadgesArray = [];
+		winnerBadgesStrings = "";
+		winnerBadges = "";
+		const winnerbagdges = (players , winner) =>{
+			console.log("winner Pagdges function ");
+			for (var i=0;i<players[winner].badges.length;i++){
+				winnerBadges = players[winner].badges[i].icon_url;
+				winnerBadgesArray.push(winnerBadges);
+				console.log("pushed padge to the winner Pagdges array");
+			}
+			console.log ("the winner badges ",winnerBadgesArray);
+			for (var j=0 ; j<winnerBadgesArray.length ;j++){
+				winnerBadgesStrings += `<div>`;
+				winnerBadgesStrings += `<img class="badgeImage col-md-12" src="${winnerBadgesArray[j]}"`;
+				winnerBadgesStrings += `</div>`;
+			}
+			$("#badges").append(winnerBadgesStrings);
+		};	
 
-		
 
-   
-
-		// $("#btn").on("click", function() {
-		// loadsanyyousif();
-  //       loadellisthomas();
-
-		    Promise.all([loadsanyyousif(), loadellisthomas()])
+		    Promise.all([loadFirstPlayer(), loadSecondPlayer()])
 		        .then((result) => {
 		            console.log("the result is : ", result);
 		            result.forEach((xhrResult) => {
@@ -118,15 +105,12 @@ $(document).ready(function() {
 		        printplayerData(players);
 		        console.log("players",players);
 		        checkForWinner(players);
-		         //    console.log("the players array is :", players);
-		         //    var pic =players[0].gravatar_url;
-		        	// console.log("pic",pic);
-		        	// printplayerData("players",players);
 		        })
 		        .catch(function(error){
 	                console.log(error);
 	            });
-		// });
+	            $("#winner").removeClass("hidden");
+		
 	});
 
 
